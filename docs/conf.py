9c028b454ec594154602357bb24afbfef0f9ed08
Martin Hunt <martinghunt@users.noreply.github.com>
2026-03-28 11:49:57 +0000

    Add Furo-based documentation site

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+project = "seqhiker"
+copyright = "2026, Martin Hunt"
+author = "Martin Hunt"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".md": "markdown",
+}
+
+master_doc = "index"
+
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
+
+html_theme = "furo"
+html_title = "seqhiker"
